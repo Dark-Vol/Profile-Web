28af3344cb11c2816055db0fa182b1b803c93f28
Dark-Vol <nv865839@gmail.com>
2026-09-18 22:38:49 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+  // allowedDevOrigins: ["26.165.10.108"],
 };
 
 export default nextConfig;
