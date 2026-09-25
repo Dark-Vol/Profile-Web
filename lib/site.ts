d0bb4297f8fa0685ce010f895142c6a36dd2ce89
Dark-Vol <nv865839@gmail.com>
2026-09-25 12:38:53 +0300

    my info

---
+++ lib/site.ts
@@ -0,0 +1,7 @@
+export const contacts = {
+  telegram: "https://t.me/VavilovNiki",
+  instagram: "",
+  email: "nv965939@gmail.com",
+  phone: "+380505799437",
+  phoneLabel: "+380 505 799 437",
+} as const;
